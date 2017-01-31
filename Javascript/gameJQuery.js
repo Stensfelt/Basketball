bb54ffb27526e4b7a4937c057d819069796c12ee
Stensfelt <92joste@live.se>
2017-01-31 14:54:23 +0100

    Tillägg av jQuery

--- Javascript/gameJQuery.js
+++ Javascript/gameJQuery.js
@@ -1,19 +1,35 @@
 function Test(){
     var x = event.clientX;     // Hämtar x-koordinater för musen
-    //var y = event.clientY;     // Hämtar y-koordinater för musen
     moveHoop(x)
 }
 
-
-
 function moveHoop(a) {
-  //alert("MouseX = " + a + ", MouseY = " + b);
   a = a + -70;
   var b = a + -50;
-  //b = b -260;
+
   document.getElementById('korg').style.marginLeft = a + "px";
-  document.getElementById('plank').style.marginLeft = b + "px";
-  //document.getElementById('basketkorg').style.marginTop = b + "px";
-  
+  document.getElementById('plank').style.marginLeft = b + "px"; 
 }
 
+$(document).ready(function(){
+  $("#basketball").click(function(){
+    randomBasketBalls();
+  });
+});
+
+function randomBasketBalls() {
+  $("#basketball").css("marginTop", "0px");
+  randomX();
+  moveBall();
+}
+
+function randomX(){
+  xCoordinate = Math.floor((Math.random() * 1100) + 1);
+  $("#basketball").css("marginLeft", xCoordinate + "px");
+}
+
+function moveBall() {
+  $("#basketball").animate({bottom: '0'}, 2000, "linear", function() {
+    
+  });
+}
