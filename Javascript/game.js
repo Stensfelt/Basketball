aa8f2e8c76d548e9ae177ea5abeeeb80b1c44ec1
Johannes Stensfelt <92joste@live.se>
2017-02-01 20:47:18 +0100

    Justeringar i gamejs

--- Javascript/game.js
+++ Javascript/game.js
@@ -1,16 +1,3 @@
-function Test(){
-    var x = event.clientX;     // Hämtar x-koordinater för musen
-    moveHoop(x)
-}
-
-function moveHoop(a) {
-  a = a + -70;
-  var b = a + -50;
-
-  $("#korg").css("marginLeft", a + "px");
-  $("#plank").css("marginLeft", b + "px");
-}
-
 $(document).ready(function(){
   $("#startGameDiv").click(function(){
     $("#startGameDiv").css("display", "none");
@@ -19,57 +6,106 @@ $(document).ready(function(){
     $("#plank").css("opacity", "initial");
     $("#basketball").css("opacity", "initial"); 
     moveBall();
-  });
-});
+  });  
 
-function randomBasketBalls() {
-  $("#basketball").css("marginTop", "0px");
-  randomX();
-  moveBall();
-}
+  var ball = $("#basketball");
+  var ballSize = {
+    height: ball.height(),
+    width: ball.width()
+  };
 
-function randomX(){
-  xCoordinate = Math.floor((Math.random() * (screenSize - 110)));
-  $("#basketball").css("marginLeft", xCoordinate + "px");
-}
+  var hoop = $("#korg");
+  var hoopSize = {
+    height: hoop.height(),
+    width: hoop.width()
+  };
 
-function moveBall() {
-  $("#basketball").animate({marginTop: '900'}, 1500, "linear", function() {
-    randomBasketBalls();
-  });
-}
+  var z = 0;
 
-$(window).keypress(function(e) {   //keypress är en inbyggd funktion som tar reda på vilken tangent som trycks på genom s.k. "keycodes", se nedan
-  var isAnimating = $("#basketball").is(':animated'); //Returnerar True om variabeln genomgår en animation
-
-  if (isAnimating == false) //Om animationen är pausad
-  {
-    if (e.keyCode == 32) //Om mellanslag trycks ner (keyCode för mellanslag är 0)
-    {
-      document.getElementById("korgDiv").onmousemove = function(){Test()};   //Sätter igång rörelsen för korg och plank
-      $("#startGameDiv").css("display", "none"); //Gör "Start"-knappen osynlig
-      moveBall();  //Kör igång animationen
-      $("#korg").css("opacity", "initial");
-      $("#plank").css("opacity", "initial");
-      $("#basketball").css("opacity", "initial");  
+  function goal(position1, size1, position2, size2) {
+    if (  ((position1.left + size1.width)  > position2.left) &&
+          ((position1.top  + size1.height) > position2.top)  &&
+          ((position2.left + size2.width)  > position1.left) &&
+          ((position2.top  + size2.height) > position1.top)) 
+    {       
+      z++;
+      $("#currentScore").text(z);
     }
   }
-  else //Om animation inte är pausad
+
+  function Test(){
+      var x = event.clientX;     // Hämtar x-koordinater för musen
+      moveHoop(x)
+  }
+  
+  function moveHoop(a) {
+    a = a + -70;
+    var b = a + -50;
+  
+    $("#korg").css("marginLeft", a + "px");
+    $("#plank").css("marginLeft", b + "px");
+  }
+  
+  function randomBasketBalls() {
+    $("#basketball").css("marginTop", "0px");
+    randomX();
+    moveBall();
+  }
+  
+  function randomX(){
+    xCoordinate = Math.floor((Math.random() * (screenSize - 110)));
+    $("#basketball").css("marginLeft", xCoordinate + "px");
+  }
+  
+  function moveBall() {
+    $("#basketball").animate({marginTop: "900"}, {
+      duration: 1500,
+      step: function(){
+        goal(ball.position(), ballSize, hoop.position(), hoopSize);
+      },
+      queue: false,
+      complete: function(){randomBasketBalls();}
+    });
+  
+  
+    //$("#basketball").animate({marginTop: '900'}, 1500, "linear", function() {
+      //randomBasketBalls();
+    //});
+  }
+
+  $(window).keypress(function(e) {   //keypress är en inbyggd funktion som tar reda på vilken tangent som trycks på genom s.k. "keycodes", se nedan
+    var isAnimating = $("#basketball").is(':animated'); //Returnerar True om variabeln genomgår en animation
+  
+    if (isAnimating == false) //Om animationen är pausad
     {
-      if (e.keyCode == 32)
+      if (e.keyCode == 32) //Om mellanslag trycks ner (keyCode för mellanslag är 0)
       {
-        $("#startGameDiv").css("display", "initial"); //Gör startknappen till sitt css-ursprungsläge
-        $("#startGameParagraph").text("Paused"); //Gör om texten från "Start" till "Paused"
-        document.getElementById("korgDiv").onmousemove = false; //Avaktiverar rörelsen för korg och plank 
-        $("#basketball").stop();  //Pausar animationen
-        $("#korg").css("opacity", "0.6");
-        $("#plank").css("opacity", "0.6");
-        $("#basketball").css("opacity", "0.6");
+        document.getElementById("korgDiv").onmousemove = function(){Test()};   //Sätter igång rörelsen för korg och plank
+        $("#startGameDiv").css("display", "none"); //Gör "Start"-knappen osynlig
+        moveBall();  //Kör igång animationen
+        $("#korg").css("opacity", "initial");
+        $("#plank").css("opacity", "initial");
+        $("#basketball").css("opacity", "initial");  
       }
     }
-});
-var screenSize = $(window).width();
+    else //Om animation inte är pausad
+      {
+        if (e.keyCode == 32)
+        {
+          $("#startGameDiv").css("display", "initial"); //Gör startknappen till sitt css-ursprungsläge
+          $("#startGameParagraph").text("Paused"); //Gör om texten från "Start" till "Paused"
+          document.getElementById("korgDiv").onmousemove = false; //Avaktiverar rörelsen för korg och plank 
+          $("#basketball").stop();  //Pausar animationen
+          $("#korg").css("opacity", "0.6");
+          $("#plank").css("opacity", "0.6");
+          $("#basketball").css("opacity", "0.6");
+        }
+      }
+  });
+  var screenSize = $(window).width();
+  
+  $(window).resize(function(){
+    screenSize = $(window).width();
+  });
 
-$(window).resize(function(){
-  screenSize = $(window).width();
 });
