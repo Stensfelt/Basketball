1c648f67f8292c3b7195227b17cfa238cdfb6fa3
EnglundAndreas <andreas95englund@gmail.com>
2017-02-09 13:24:18 +0100

    Fixat saveScore

--- saveScore.php
+++ saveScore.php
@@ -7,12 +7,12 @@ $finalScore = mysqli_real_escape_string($conn, $_POST['phpScore']);
 
 if (isset($_SESSION['username'])) {
   $username = $_SESSION['username'];
-} else{
+} else {
   mysqli_close($conn);
 }
-$sql = "SELECT * FROM leaderboards ORDER BY score DESC LIMIT 10";
-$result = $conn->query($sql) or trigger_error($conn->error."[$sql]");
-$row = $result->fetch_assoc();
+
+
+$sql = "INSERT INTO leaderboards (username, score) VALUES ('$username', '$finalScore')";
 
 if (!mysqli_query($conn, $sql)){
     die('error ' . mysqli_error($conn));
