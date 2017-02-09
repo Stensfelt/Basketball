88edec3ccac53a50511b9ca34352f9d7552020ea
EnglundAndreas <andreas95englund@gmail.com>
2017-02-09 12:21:41 +0100

    connect ändrad DB

--- connect.php
+++ connect.php
@@ -1,7 +1,6 @@
 <?php
 
-//$conn = mysqli_connect('localhost', 'id692526_lushball_user', 'lushball05', 'id692526_lushball_db');
-$conn = mysqli_connect('localhost', 'root', '', 'test');
+$conn = mysqli_connect('localhost', 'id692526_lushball_user', 'lushball05', 'id692526_lushball_db');
 
 if (!$conn){
   die("the connection failed".mysqli_connect_errno());
